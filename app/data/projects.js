8c708a3569271f42d3ae6892bed859d830237d95
karinfdez <karinfdez@yahoo.com>
2025-08-03 22:43:27 -0400

    Add list of projects

---
+++ app/data/projects.js
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+      "title": "Class Action",
+      "description": "A dashboard to manage class action lawsuits.",
+      "image": "/images/projects-images/class-action.png",
+      "link": "https://github.com/karinfdez/class-action",
+    },
+    {
+      "title": "Email Service",
+      "description": "An email service built using Cloudflare AI Agents.",
+      "image": "/images/projects-images/email-service.png",
+      "link": "https://github.com/karinfdez/email-service",
+    },
+    {
+      "title": "Chatbot",
+      "description": "A chatbot to answer questions about my portfolio.",
+      "image": "/images/projects-images/langchain-chatbot.png",
+      "link": "https://langchain-chatbot-pi.vercel.app/",
+    }
+  ]
