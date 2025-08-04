484f10d4a1968ab11e0c2cc1ac362bd438e5d920
karinfdez <karinfdez@yahoo.com>
2025-08-03 23:07:31 -0400

    Update projects description

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,19 +1,19 @@
 export const projects = [
     {
       "title": "Class Action",
-      "description": "A dashboard to manage class action lawsuits.",
+      "description": "A dashboard to identify and manage potential class action members.",
       "image": "/images/projects-images/class-action.png",
       "link": "https://github.com/karinfdez/class-action",
     },
     {
       "title": "Email Service",
-      "description": "An email service built using Cloudflare AI Agents.",
+      "description": "An AI-powered email assistant built with Cloudflare AI Agents.",
       "image": "/images/projects-images/email-service.png",
       "link": "https://github.com/karinfdez/email-service",
     },
     {
       "title": "Chatbot",
-      "description": "A chatbot to answer questions about my portfolio.",
+      "description": "An AI-powered assistant built with Langchain that answers questions using internal company data.",
       "image": "/images/projects-images/langchain-chatbot.png",
       "link": "https://langchain-chatbot-pi.vercel.app/",
     }
