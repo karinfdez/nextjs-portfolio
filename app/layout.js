516036c5fccb82e68bb6ba4dd8f1b378627c9404
karinfdez <karinfdez@yahoo.com>
2025-08-03 21:28:22 -0400

    Update the menu on mobile

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,8 @@ export default function RootLayout({ children }) {
       <body className={`${fraunces.className} min-h-full antialiased flex flex-col`}>
         <header className="w-full flex justify-center md:justify-end py-8 px-4 md:pr-12 text-xl lg:text-2xl max-w-6xl mx-auto relative">
           <AnimatedDots className="absolute left-0 top-0 scale-90" colorClass="bg-orange-500" isClickable={true} />
-          <nav className="space-x-10">
+          <nav className="space-x-6">
+            <Link href="/" className="hover:text-orange-400 transition-colors duration-200 md:hidden">Home</Link>
             <Link href="/about" className="hover:text-orange-400 transition-colors duration-200">About</Link>
             <Link href="/projects" className="hover:text-orange-400 transition-colors duration-200">Projects</Link>
             <Link href="/contact" className="hover:text-orange-400 transition-colors duration-200">Contact</Link>
