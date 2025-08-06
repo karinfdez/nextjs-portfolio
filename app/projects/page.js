c42a0f9743e4b1ebac6024e8c19c22500a821895
karinfdez <karinfdez@yahoo.com>
2025-08-05 20:46:41 -0400

    Add description of projects

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,14 +1,19 @@
-export const metadata = {
-  title: 'Projects – My Portfolio',
-};
 
+"use client";
+import AnimatedReveal from "../components/AnimatedReveal";
 import ProjectsCarousel from "../components/ProjectsCarousel";
 import { projects } from "../data/projects";
 
 export default function ProjectsPage() {
   return (
     <main className="flex flex-col items-center justify-center flex-1 overflow-hidden px-4 space-y-8">
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">Projects</h1>
+      <AnimatedReveal delay={0.1}>
+        <h1 className="w-full text-center text-4xl md:text-6xl font-bold mb-4">Projects</h1>
+        
+        <p className="text-gray-300 text-lg leading-relaxed text-center max-w-xl mx-auto mt-2">
+          {`A few highlights of the personal projects I’ve built, combining automation, AI, and clean frontend architecture.`}
+        </p>
+      </AnimatedReveal>
       <ProjectsCarousel
         projects={projects}
       />
