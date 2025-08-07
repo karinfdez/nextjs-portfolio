746273eb9cee009a72766f07c71d27925bc51072
karinfdez <karinfdez@yahoo.com>
2025-08-07 16:53:26 -0400

    Add double geometric figues

--- app/components/FloatingShapesBG.jsx
+++ app/components/FloatingShapesBG.jsx
@@ -27,29 +27,38 @@ const FloatingShapes = () => {
         </svg>
       </motion.div>
 
-      {/* Orange triangle (center-right) */}
+      {/* Triangle pair (center-right) */}
       <motion.div
         className="absolute top-1/4 lg:top-1/2 right-10 3xl:right-28"
         variants={float}
         animate="animate"
-        transition={{ duration: 12, repeat: Infinity, ease: 'easeInOut' }} 
+        transition={{ duration: 12, repeat: Infinity, ease: 'easeInOut' }}
       >
+        {/* Main triangle */}
         <svg width="180" height="160" viewBox="0 0 180 160" className="opacity-30">
-          <polygon
-            points="90,10 170,150 10,150"
-            fill="none"
-            className="stroke-[2] stroke-orange-500/70"
-          />
+          <polygon points="90,10 170,150 10,150" fill="none" className="stroke-[2] stroke-orange-500/70" />
         </svg>
       </motion.div>
 
-      {/* Orange hexagon (bottom-left) */}
+      {/* Secondary triangle (smaller + slight offset) */}
+      <motion.div
+        className="absolute top-[30%] lg:top-[52%] right-[80px] 3xl:right-[150px]"
+        animate={{ y: [0, -8, 0], rotate: [0, -10, 0] }}
+        transition={{ duration: 15, repeat: Infinity, ease: 'easeInOut', delay: 0.5 }}
+      >
+        <svg width="140" height="125" viewBox="0 0 180 160" className="opacity-20">
+          <polygon points="90,10 170,150 10,150" fill="none" className="stroke-[1.5] stroke-orange-500/60" />
+        </svg>
+      </motion.div>
+
+      {/* Hexagon pair (bottom-left) */}
       <motion.div
         className="absolute bottom-10 lg:bottom-20 left-16 3xl:left-28"
-        variants={float} 
+        variants={float}
         animate="animate"
-        transition={{ duration: 14, repeat: Infinity, ease: 'easeInOut' }} // different tempo
+        transition={{ duration: 14, repeat: Infinity, ease: 'easeInOut' }}
       >
+        {/* Main hexagon */}
         <svg width="160" height="160" viewBox="0 0 160 160" className="opacity-15">
           <path
             d="M80 10 L140 45 L140 115 L80 150 L20 115 L20 45 Z"
@@ -59,7 +68,22 @@ const FloatingShapes = () => {
         </svg>
       </motion.div>
 
-      {/* Soft blurred blob (bonus accent, top-right) */}
+      {/* Secondary hexagon (smaller + offset) */}
+      <motion.div
+        className="absolute bottom-[85px] lg:bottom-[130px] left-[95px] 3xl:left-[165px]"
+        animate={{ y: [0, -6, 0], rotate: [0, 8, 0] }}
+        transition={{ duration: 11, repeat: Infinity, ease: 'easeInOut', delay: 0.3 }}
+      >
+        <svg width="120" height="120" viewBox="0 0 160 160" className="opacity-20">
+          <path
+            d="M80 20 L132 50 L132 110 L80 140 L28 110 L28 50 Z"
+            fill="none"
+            className="stroke-[1.5] stroke-orange-500/70"
+          />
+        </svg>
+      </motion.div>
+
+      {/* Soft blurred blob (top-right accent) */}
       <motion.div
         className="absolute -top-24 right-[-60px] w-[380px] h-[380px] rounded-full bg-orange-500/10 blur-[120px]"
         animate={{ y: [-10, 10, -10] }}
@@ -67,6 +91,6 @@ const FloatingShapes = () => {
       />
     </div>
   );
-}
+};
 
 export default FloatingShapes;
