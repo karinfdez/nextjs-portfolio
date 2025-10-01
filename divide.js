547b3d7c1fcc6628dba14cdd287109fa96200936
karinfdez <karinfdez@yahoo.com>
2025-10-01 16:38:06 -0400

    Update greptile

--- divide.js
+++ divide.js
@@ -2,7 +2,7 @@ function divide(a, b) {
     return a / b; // Missing division by zero check
 }
 
-// @greptile
+// @greptileai
 function divideNumber2(a, b) {
     return a / b; // Missing division by zero check
 }
